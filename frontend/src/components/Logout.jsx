c4967aa27d15b351d735899e520b1eae6973cf3c
Alpharex121 <arpitkoshta08@gmail.com>
2024-04-30 00:54:04 +0530

    fixing logout 43

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -38,6 +38,7 @@ const Logout = () => {
       console.log(data);
       if (data.status == 200) {
         console.log(cookies);
+        console.log(document.cookie);
         removeCookie("jwt");
         alert("Logged out seccuessfull");
         Navigate("/");
