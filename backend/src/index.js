13d3256bf40521355da5bc46e83b9afc5f2894c6
Alpharex121 <arpitkoshta08@gmail.com>
2024-04-30 14:14:00 +0530

    fixing logout 55

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Allow specific HTTP methods
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, OPTIONS, DELETE"
+  );
 
   // Allow specific headers to be sent in the request
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
