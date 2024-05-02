8d4d8680397cd28c68a6c78e79fbc0f31ab2db1b
Alpharex121 <arpitkoshta08@gmail.com>
2024-05-02 15:39:16 +0530

    updated cors 5

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -24,7 +24,6 @@ router.post("/", async (req, res) => {
       res.cookie("jwt", token, {
         expires: new Date(Date.now() + 3000000),
         secure: true,
-        domain: "ggitsstudentsapi.vercel.app",
         sameSite: "none",
       });
       res.status(200).send(userCred);
@@ -43,7 +42,6 @@ router.get("/logout", auth, async (req, res) => {
     if (token) {
       req.user.tokens = [];
       res.clearCookie("jwt", {
-        domain: "ggitsstudentsapi.vercel.app",
         sameSite: "none",
         secure: true,
         path: "/",
